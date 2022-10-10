67a704b682b73700c0f9fcbbbac2e767a50c8aaa
Natali <n7483205@ya.ru>
2022-10-10 21:09:24 +0700

    days off

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number >7) 
+Console.WriteLine($"{number} Число не верно - в неделе 7 дней.");
+if (number <1)
+Console.WriteLine($"{number} Число не верно - в неделе 7 дней.");
+
+else if (number == 1) Console.WriteLine("Понедельник - рабочий день.");
+else if (number == 2) Console.WriteLine("Вторник - рабочий день.");
+else if (number == 3) Console.WriteLine("Среда - рабочий день.");
+else if (number == 4) Console.WriteLine("Четверг - рабочий день.");
+else if (number == 5) Console.WriteLine("Пятница - рабочий день.");
+else if (number == 6) Console.WriteLine("Суббота - выходной день.");
+else if (number == 7) Console.WriteLine("Воскресенье - выходной день.");
